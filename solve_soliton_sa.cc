a0c72c22df51b46f2b457e0ba66737f5d2660930
useakat@kekcc <useakat@gmail.com>
2016-06-15 16:36:06 +0900

    solvesoliton_sa.cc debugged

--- solve_soliton_sa.cc
+++ solve_soliton_sa.cc
@@ -1,4 +1,4 @@
-//Simulated Annealing kink 
+//Simulated Annealing kink
 #include <cstdio>
 #include <cmath>
 #include <ctime>
@@ -6,10 +6,10 @@
 #include <cstring>
 #include "mersenne.h"
 
-#define PI 3.1443926584 //  
+#define PI 3.1443926584 //  
 #define MESH 70 // number of division of mesh
 #define MIN -10.0 // minimum of x, y
-#define MAX 10.0 // maximum of x, y  
+#define MAX 10.0 // maximum of x, y
 #define START 200001 // total number of the calculation
 #define TSTART 5 // Permit the transition to the direction of raising cost in every TSTART
 #define KAKUNIN 1000 // Output Energy and Charge in every kakunin
@@ -17,12 +17,12 @@
 using namespace std;
 
 int M1_2=(MESH+1)*(MESH+1);
-double h; // 
-double rh; // inverse of h 
+double h; //
+double rh; // inverse of h
 double tF[MESH],dx_F[MESH]; // average and derivative of the Phi field
-double v=PI;
-double mass=1.0;
-double lam=mass/v/v; // lambda
+double v = PI;
+double mass = 1.0;
+double lam = mass/v/v; // lambda
 double Ed[MESH]; // energy density
 double cd[MESH]; // charge density
 double memo_n[START/KAKUNIN+1]; // store charge history
@@ -30,13 +30,13 @@ double memo_E[START/KAKUNIN+1]; // store energy history
 
 struct{
   double F[MESH+1]; // configuration of Phi field
-} od, nd;
+} od,nd;
 
 // definition of the initial configuration
-double func_F(double x); 
+double func_F(double x);
 
 // tarapezoidal formula for the multiple integration
-double trapezoidal2(void); 
+double trapezoidal2(void);
 
 // integration for calculations of the tpological charge
 double trapezoidal_N(void); //
@@ -46,13 +46,13 @@ void ransu(void);
 
 int main(void)
 {
-  double oE, nE; // old energy, new energy     
+  double oE, nE; // old energy, new energy
   double on, nn; // old charge, new charge
   int count = START; // number of calculation
-  int tcount = TSTART; // 
+  int tcount = TSTART; //
   int dcount = 0; // the number of replacement of configurations
   int i; // for loop
-  FILE *data, *fp; 
+  FILE *data, *fp;
   char name[80]; // file name of field configurations
   char name2[80]; // information of the energy and charge density
   int number=0; // the number of save
@@ -65,14 +65,15 @@ int main(void)
   for (i = MESH; i >= 0; i--){
     od.F[i] = func_F(MIN +i*h);
   }
+
   // boundary condition
-  od.F[0] = -v; 
-  od.F[MESH/2] = 0; 
+  od.F[0] = -v;
+  od.F[MESH/2] = 0;
   od.F[MESH] = v;
 
   nd = od;
 
-  // define the paritial derivative of the phi field 
+  // define the paritial derivative of the phi field
   for(i = MESH -1; i >= 0; i--){
     dx_F[i] = (nd.F[i+1] -nd.F[i])*rh;
   }
@@ -107,7 +108,7 @@ int main(void)
   fclose(data);
 
   // change scaler field according to random numbers
-  init_genrand((unsigned)time(NULL)); 
+  init_genrand((unsigned)time(NULL));
   do{
 
     // for check
@@ -119,7 +120,7 @@ int main(void)
     }
     if(count%10000 == 0) printf("\n  %d\n",count);
 
-    // perturb configurations according to random numbers       
+    // perturb configurations according to random numbers
     if(count > (START*0.4) ){ double tmp = M1_2*1.2;
       for(i = tmp; i >= 0; i--){
 	ransu();
@@ -139,8 +140,8 @@ int main(void)
     }
 
     // boundary condition
-    nd.F[0] = -v; 
-    nd.F[MESH/2] = 0; 
+    nd.F[0] = -v;
+    nd.F[MESH/2] = 0;
     nd.F[MESH] = v;
 
     // define partial derivative of phi field
@@ -154,7 +155,7 @@ int main(void)
     }
 
     // change of the topological charge
-    nn = trapezoidal_N();//b/(4*pi)  
+    nn = trapezoidal_N(); //b/(4*pi)
     // calcualte energy with the changed configuration
     nE = trapezoidal2();
 
@@ -168,7 +169,7 @@ int main(void)
       on = nn;
       tcount = TSTART;
       dcount++;
-      od = nd; // update all the configurations here         
+      od = nd; // update all the configurations here
     }
     if(oE != nE)  tcount--;
     if(count%5000 == 0){
@@ -177,7 +178,7 @@ int main(void)
       sprintf(name2,"kink_%d_Bz.txt" ,number);
 
       // for saving calculation results
-      data = fopen(name,"w"); 
+      data = fopen(name,"w");
       for(i=MESH; i>=0; i--){
 	fprintf(data,"%lf\t%lf\n", MIN+i*h, nd.F[i]);
       }
@@ -192,14 +193,13 @@ int main(void)
   }while(count>0);
 
   // draw oE,on vs. count graph
-  data = fopen("kink_c-E.txt","w"); // for saving calculation results 
+  data = fopen("kink_c-E.txt","w"); // for saving calculation results
   for (i=START/KAKUNIN;i>0;i--){
     fprintf(data,"%d\t%lf\t%lf\n", i*KAKUNIN, memo_E[i], memo_n[i]);
   }
   fclose(data);
   return 0;
 }
-
 
 // trapezoidal formula of multiple integral
 double trapezoidal2(void) {
@@ -213,7 +213,7 @@ double trapezoidal2(void) {
     Ed[i] = (0.5*dx_F[i]*dx_F[i] +lam/4.0*(tF[i]*tF[i] -v*v)*(tF[i]*tF[i] -v*v))*s;
     S += Ed[i]*h;
   }
-  return S; 
+  return S;
 }
 
 // integral for calculating the topological charge
@@ -237,7 +237,7 @@ void ransu(void)
   int random; // for storing random variables
   double leng = 0.001; // initial amplitude parameter
 
-  // change random variables discretely        
+  // change random variables discretely
   random = genrand_int31()%((MESH-1)*(MESH-1)*2);
   rk = random%2;
   random = (random -rk)*0.5;
